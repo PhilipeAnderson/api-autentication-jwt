a5dd34c2ff180d457bca69308570e2a9a507ffa6
philipe_kraken <philipeacampos@hotmail.com>
2022-04-27 21:18:33 -0300

    No bugs so far. Ready for continue...

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
 import express, { Request, Response, NextFunction} from 'express';
-import { bands } from './database/bands';
 
 const app = express();
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({foo: 'bar'})
-})
-
-
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send(bands)
 })
 
 app.listen(3333, () => {
