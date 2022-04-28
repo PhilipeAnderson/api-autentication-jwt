9f53eae85affe5ef3b8bdf0a3051fb46ce9cb174
philipe_kraken <philipeacampos@hotmail.com>
2022-04-27 23:54:25 -0300

    Post working

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response, NextFunction} from 'express';
+import morgan from 'morgan';
 import userRoute from './routes/users';
 
+//Application
 const app = express();
-app.use(userRoute)
 
+//Settings
+app.use(express.json());  //Parse data in Json
+app.use(morgan('dev'));   //Return status code for Debug
 
+//Routes
+app.use(userRoute);
+
+//Server
 app.listen(3333, () => {
   console.log('Server Working | Port:3333')
 })
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -6,17 +6,20 @@ const usersRoute = Router();
 //get /users
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
   const users = [{ name: 'Philipe' }];
-  res.status(StatusCodes.OK).json(users);
+  res.status(200).json(users);
 });
 
 //get /users/:uuid
 usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
   const uuid = req.params.uuid;
-  res.status(200).send({ uuid });
+  res.status(StatusCodes.OK).send({ uuid });
 });
 
 //post /users
-
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+  const newUser = req.body
+  res.status(StatusCodes.CREATED).send(newUser)
+});
 
 
 //put /users/:uuid
